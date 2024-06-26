fb9958cee82d4e7184fc029e8d52d403f8173360
s19835 <s19835@sci.pdn.ac.lk>
2024-06-26 22:14:28 +0530

    response added

--- app/main.js
+++ app/main.js
@@ -5,7 +5,13 @@ console.log("Logs from your program will appear here!");
 
 
 const server = net.createServer((socket) => {
-    socket._write("HTTP/1.1 200 OK\r\n\r\n");
+
+    socket.addListener("data", (d) => {
+        const data = d;
+        if (data.includes('/')) socket._write("HTTP/1.1 200 OK\r\n\r\n");
+        else { socket._write("HTTP/1.1 404 Not Found\r\n\r\n"); }
+    });
+
     socket.on("close", () => {
         socket.end();
     });
