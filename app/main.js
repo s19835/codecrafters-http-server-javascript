971c9fc81b87759a729733f3e8260e8e689ee7a5
s19835 <s19835@sci.pdn.ac.lk>
2024-06-27 14:41:57 +0530

    response for /echo/{str}

--- app/main.js
+++ app/main.js
@@ -7,40 +7,39 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
-        //get the user request and parse as string
-        let request = data.toString();
-        request = request.split('\r\n');
-
-        //split the request by '\r\n' and get the first which is request line
-        const requestLine = request[0];
-        
-        //then get the url from the request line
-        const urlPath = requestLine.split(' ')[1];
-
-        let response;
-        //get the content from the url
-        let str = urlPath.substring(6);
-
-        //find the user-agent from the array request
-        const agent = request.find((user) => {
-            user.startsWith('User-Agent');
-        })
-
-        const userAgent = agent.split(': ')[1];
-
-        if (urlPath.startsWith('/echo/') || urlPath === '/') {
-            response = '200 OK';
-            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
-        } 
-        else if (userAgent) {
-            response = '200 OK';
-            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n${userAgent}`);
-        }
-        else {
-            response = '404 Not Found';
-            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
-        }
-
+        socket.on("data", (data) => {
+            const request = data.toString();
+            const requestLines = request.split('\r\n');
+            const requestLine = requestLines[0];
+            const headers = requestLines.slice(1, requestLines.indexOf(''));
+            const urlPath = requestLine.split(' ')[1];
+    
+            let response;
+            let responseBody;
+            let statusCode;
+    
+            // Extract the User-Agent header
+            const userAgentHeader = headers.find(header => header.toLowerCase().startsWith('user-agent:'));
+            const userAgent = userAgentHeader ? userAgentHeader.split(': ')[1] : '';
+    
+            if (urlPath === '/user-agent') {
+                statusCode = '200 OK';
+                responseBody = userAgent;
+            } else {
+                statusCode = '404 Not Found';
+                responseBody = 'Not Found';
+            }
+    
+            const responseHeaders = `HTTP/1.1 ${statusCode}\r\n` +
+                                    `Content-Type: text/plain\r\n` +
+                                    `Content-Length: ${responseBody.length}\r\n` +
+                                    `\r\n`;
+    
+            response = responseHeaders + responseBody;
+    
+            socket.write(response);
+        });
+    
         
     })
     
