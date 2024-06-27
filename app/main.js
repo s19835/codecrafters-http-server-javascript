358fc9e82e93967594a9e850040228988bb964bd
s19835 <s19835@sci.pdn.ac.lk>
2024-06-27 11:57:36 +0530

    response for /echo/{str}

--- app/main.js
+++ app/main.js
@@ -11,14 +11,14 @@ const server = net.createServer((socket) => {
         const requestLine = request.split('\r\n')[0];
         const urlPath = requestLine.split(' ')[1];
         let response;
-        let str = urlPath.split('/');
+        let str = urlPath.substring(6);
         if (urlPath.startsWith('/echo/')) {
             response = '200 OK';
         } else {
             response = '404 Not Found'
         }
         
-        socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str[1].length}\r\n\r\n${str[1]}`);
+        socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
     })
     
 
