a251370542e2193536944ead855cce4cb635c47a
s19835 <s19835@sci.pdn.ac.lk>
2024-06-27 10:56:54 +0530

    check request for validation

--- app/main.js
+++ app/main.js
@@ -7,9 +7,12 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
-        if (data.includes('/')) socket._write("HTTP/1.1 200 OK\r\n\r\n");
-        else { socket._write("HTTP/1.1 404 Not Found\r\n\r\n"); }
+        const request = data.toString();
+        const urlPath = request.split('\r\n')[1];
+        const response = (urlPath === '/') ? '200 OK' : '404 Not Found';
+        socket._write(`HTTP/1.1 ${response}\r\n\r\n`);
     })
+    
 
     socket.on("close", () => {
         socket.end();
