62db67153b756cf61b42b718e686453427a45019
s19835 <s19835@sci.pdn.ac.lk>
2024-06-27 14:39:00 +0530

    response for /echo/{str}

--- app/main.js
+++ app/main.js
@@ -7,20 +7,41 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {
-        const request = data.toString();
-        const requestLine = request.split('\r\n')[0];
-        const headerLine = request.split('\r\n')[1];
-        const userAgent = headerLine.split('\r\n')[1];
+        //get the user request and parse as string
+        let request = data.toString();
+        request = request.split('\r\n');
+
+        //split the request by '\r\n' and get the first which is request line
+        const requestLine = request[0];
+        
+        //then get the url from the request line
         const urlPath = requestLine.split(' ')[1];
+
         let response;
+        //get the content from the url
         let str = urlPath.substring(6);
+
+        //find the user-agent from the array request
+        const agent = request.find((user) => {
+            const user = user.startsWith('User-Agent');
+        })
+
+        const userAgent = agent.split(': ')[1];
+
         if (urlPath.startsWith('/echo/') || urlPath === '/') {
             response = '200 OK';
-        } else {
+            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
+        } 
+        else if (userAgent) {
+            response = '200 OK';
+            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n${userAgent}`);
+        }
+        else {
             response = '404 Not Found';
+            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
         }
 
-        socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${userAgent.substring(11)}`);
+        
     })
     
 
