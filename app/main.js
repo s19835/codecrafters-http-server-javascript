2e0089742b0b864500d08a44cae58a44e5cb978c
s19835 <s19835@sci.pdn.ac.lk>
2024-06-28 12:49:58 +0530

    added code for POST request

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import * as net from "net";
 import { join } from "path";
-import { readFile } from "fs";
+import { readFile, writeFile } from "fs";
 
 
 console.log("Logs from your program will appear here!");
@@ -17,8 +17,6 @@ path.forEach((flag, index) => {
         directory = path[index+1];
     }
 });
-
-console.log(directory);
 
 
 const server = net.createServer((socket) => {
@@ -39,42 +37,54 @@ const server = net.createServer((socket) => {
         let str = urlPath;
         str = urlPath.substring(6);
         
-        console.log(urlPath);
+        const requestMethod = requestLine.split(' ')[0];
 
 
-        if (urlPath.startsWith('/echo/')) {
-            response = '200 OK';
-            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
-        } 
-        else if (urlPath === '/') {
-            response = '200 OK';
-            socket._write(`HTTP/1.1 ${response}\r\n\r\n`);
+        if (requestMethod === 'GET') {
+            if (urlPath.startsWith('/echo/')) {
+                response = '200 OK';
+                socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
+            } 
+            else if (urlPath === '/') {
+                response = '200 OK';
+                socket._write(`HTTP/1.1 ${response}\r\n\r\n`);
+            }
+            else if (urlPath === '/user-agent') {
+                response = '200 OK';
+                socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+                
+            }
+            else if (urlPath.startsWith('/files')) {
+                 //create the path with directory and urlPath
+                const path = join(directory, urlPath.split('/')[2]);
+                readFile(path, (err, data) => {
+                    if (data) {
+                        response = '200 OK';
+                        socket._write(`HTTP/1.1 ${response}\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
+                    }
+                    else if (err) {
+                        response = '404 Not Found';
+                        socket._write(`HTTP/1.1 ${response}\r\n\r\n`);
+                    }
+                });
+                //HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: 14\r\n\r\nHello, World!
+            }
+            else {
+                response = '404 Not Found';
+                socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
+            }
         }
-        else if (urlPath === '/user-agent') {
-            response = '200 OK';
-            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+        else if (requestMethod === 'POST') {
+            //codes for POST requests
             
-        }
-        else if (urlPath.startsWith('/files')) {
-             //create the path with directory and urlPath
+            const data = requestArray[5];
             const path = join(directory, urlPath.split('/')[2]);
-            readFile(path, (err, data) => {
-                if (data) {
-                    response = '200 OK';
-                    socket._write(`HTTP/1.1 ${response}\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n${data}`);
-                }
-                else if (err) {
-                    response = '404 Not Found';
-                    socket._write(`HTTP/1.1 ${response}\r\n\r\n`);
-                }
-            });
-            //HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: 14\r\n\r\nHello, World!
-        }
-        else {
-            response = '404 Not Found';
-            socket._write(`HTTP/1.1 ${response}\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`);
-        }
 
+            writeFile(path, data, (err) => {
+                if (err) socket._write(`HTTP/1.1 404 Not Found\r\n\r\n`);
+                else socket._write(`HTTP/1.1 201 Created\r\n\r\n`);
+            })
+        }
        
     })
     
